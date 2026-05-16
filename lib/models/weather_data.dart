7d1cf97807cfe810e11ed7cb5c92b0af066ba449
Numan Çırpan <numanccirpan@gmail.com>
2026-05-16 16:01:08 +0300

    Add OpenWeather JSON mapping to WeatherData

--- lib/models/weather_data.dart
+++ lib/models/weather_data.dart
@@ -31,4 +31,63 @@ class WeatherData {
       pressure: '1014 hPa',
     );
   }
+
+  factory WeatherData.fromOpenWeatherJson(Map<String, dynamic> json) {
+    final main = json['main'] as Map<String, dynamic>;
+    final wind = json['wind'] as Map<String, dynamic>?;
+    final weatherList = json['weather'] as List<dynamic>;
+    final weather = weatherList.first as Map<String, dynamic>;
+
+    final tempMin = (main['temp_min'] as num).round();
+    final tempMax = (main['temp_max'] as num).round();
+    final humidityValue = main['humidity'] as num;
+    final feelsLikeValue = (main['feels_like'] as num).round();
+    final pressureValue = main['pressure'] as num;
+    final windSpeedValue = ((wind?['speed'] as num?) ?? 0) * 3.6;
+
+    return WeatherData(
+      cityName: json['name'] as String? ?? 'Bilinmeyen Şehir',
+      dateText: _formatTurkishDate(DateTime.now()),
+      conditionText: _capitalize(weather['description'] as String? ?? 'Bilinmiyor'),
+      temperatureRange: '$tempMin° - $tempMax°',
+      humidity: '%${humidityValue.round()}',
+      windSpeed: '${windSpeedValue.round()} km/h',
+      feelsLike: '$feelsLikeValue°',
+      pressure: '${pressureValue.round()} hPa',
+    );
+  }
+
+  static String _capitalize(String value) {
+    if (value.isEmpty) return value;
+    return value[0].toUpperCase() + value.substring(1);
+  }
+
+  static String _formatTurkishDate(DateTime date) {
+    const months = [
+      'Ocak',
+      'Şubat',
+      'Mart',
+      'Nisan',
+      'Mayıs',
+      'Haziran',
+      'Temmuz',
+      'Ağustos',
+      'Eylül',
+      'Ekim',
+      'Kasım',
+      'Aralık',
+    ];
+
+    const weekdays = [
+      'Pazartesi',
+      'Salı',
+      'Çarşamba',
+      'Perşembe',
+      'Cuma',
+      'Cumartesi',
+      'Pazar',
+    ];
+
+    return '${date.day} ${months[date.month - 1]} ${date.year}, ${weekdays[date.weekday - 1]}';
+  }
 }
