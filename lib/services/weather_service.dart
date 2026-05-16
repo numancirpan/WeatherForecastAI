9df0ddc004b5c9c4388e4025fd88829f7dd49d12
Numan Çırpan <numanccirpan@gmail.com>
2026-05-16 15:59:54 +0300

    Prepare WeatherService for OpenWeather API

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,9 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../app_settings.dart';
 import '../models/weather_data.dart';
 
 class WeatherService {
   Future<WeatherData> getWeatherByCity(String cityName) async {
-    await Future.delayed(const Duration(milliseconds: 500));
+    if (!AppSettings.hasWeatherApiKey) {
+      await Future.delayed(const Duration(milliseconds: 500));
+      return WeatherData.sampleKocaeli();
+    }
 
-    return WeatherData.sampleKocaeli();
+    final uri = Uri.parse(
+      '${AppSettings.weatherApiBaseUrl}/weather?q=$cityName&appid=${AppSettings.weatherApiKey}&units=metric&lang=tr',
+    );
+
+    final response = await http.get(uri);
+
+    if (response.statusCode != 200) {
+      throw Exception('API error: ${response.statusCode}');
+    }
+
+    final Map<String, dynamic> json = jsonDecode(response.body) as Map<String, dynamic>;
+    return WeatherData.fromOpenWeatherJson(json);
   }
 }
